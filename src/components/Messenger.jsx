a8483ea3145041aa1bd0d0332c087bf2ee8be7db
Kaushal273 <kaushalyadav536@gmail.com>
2023-08-22 23:23:11 +0530

    initial commit

---
+++ src/components/Messenger.jsx
@@ -0,0 +1,14 @@
+import { Box, Typography} from '@mui/material';
+
+//components
+import LoginDialog from './account/LoginDialog';
+
+const Messenger = () => {
+    return(
+        <Box>
+            <Typography>Hello from Messenger</Typography>
+        </Box>
+    )
+}
+
+export default Messenger;
---
+++ src/components/account/LoginDialog.jsx
@@ -0,0 +1,11 @@
+import { Typography } from "@mui/material";
+
+
+
+const LoginDialog = () => {
+    return(
+        <Typography>Hello from login</Typography>
+    )
+}
+
+export default LoginDialog;
